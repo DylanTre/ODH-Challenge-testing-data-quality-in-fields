86baa10d50d2b77a1e6cc5f1133db21c908e5ee9
Rangatin <jumsts@gmail.com>
2023-11-15 13:33:47 +0100

    Add lombok and json parse dependencies

---
+++ ValidationComponent/src/main/java/it/unibz/InputData.java
@@ -0,0 +1,8 @@
+package it.unibz;
+
+import com.fasterxml.jackson.annotation.JsonValue;
+
+public class InputData {
+    @JsonValue
+    public int number;
+}
--- ValidationComponent/src/main/java/it/unibz/Validator.java
+++ ValidationComponent/src/main/java/it/unibz/Validator.java
@@ -5,12 +5,34 @@ import org.yaml.snakeyaml.Yaml;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.Map;
 
 public class Validator {
-    public void loadRules() throws FileNotFoundException {
+
+    public static final String RULE_CONFIG_YML = "rule-config.yml";
+
+    public Map<String, Object> loadRules() throws FileNotFoundException {
         var yaml = new Yaml();
-        var inputStream = new FileInputStream(new File("rule-config.yml"));
-        var obj = yaml.load(inputStream);
-        System.out.println(obj);
+        var inputStream = new FileInputStream(RULE_CONFIG_YML);
+
+        return yaml.load(inputStream);
+    }
+
+    /*
+     * Data to validate comes as a JSON
+     */
+    public void validate() {
+        try {
+            var validationRules = loadRules();
+
+
+
+
+
+
+        } catch (FileNotFoundException e) {
+            // better to log for now just sysout
+            System.out.println(e.getStackTrace());
+        }
     }
 }
