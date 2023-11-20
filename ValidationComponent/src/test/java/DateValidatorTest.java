2fd521acdc51f1ade814561cbd69fed6a5425c11
Rangatin <jumsts@gmail.com>
2023-11-20 14:04:06 +0100

    Add new DateValidator validation functionality and tests

--- ValidationComponent/src/test/java/DateValidatorTest.java
+++ ValidationComponent/src/test/java/DateValidatorTest.java
@@ -4,31 +4,54 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.FileNotFoundException;
-import java.time.LocalDate;
+import java.time.Clock;
+import java.time.DayOfWeek;
+import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
-import java.util.Date;
-import java.util.Map;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class DateValidatorTest {
 
-    public DateValidator dateValidator;
-    public ConfigParser config = new ConfigParser();
+    private Clock clock;
+    private DateValidator dateValidator;
 
     @Before
     public void setUp() throws FileNotFoundException {
-        config = new ConfigParser();
+        clock = Clock.fixed(Instant.parse("2023-11-20T12:00:00Z"), ZoneId.of("UTC"));
+        ConfigParser config = new ConfigParser();
         config.loadRules(ConfigParser.TEST_RULE_CONFIG_YML);
         dateValidator = new DateValidator(config.getRulesForSingleInputDataByKey("date"));
     }
 
     @Test
-    public void whenValidateDateThenFormatMustBeExactAsInRuleSpecified() {
+    public void whenValidateDateThenFormatMustBeExactAsRuleSpecified() {
         String inputStringDate = "19/11/2023 12:00:00";
         DateTimeFormatter acceptedFormat = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
 
         assertTrue(dateValidator.isFormatValid(inputStringDate, acceptedFormat));
+    }
+
+    @Test
+    public void whenValidateDateThenDayOfWeekMustBeExactAsRuleSpecified() {
+        assertTrue(dateValidator.isDateOfWeekValid(LocalDateTime.now(clock), DayOfWeek.MONDAY));
+    }
+
+    @Test
+    public void whenValidateBeforeDateThenDateMustBeBeforeRuleSpecifiedDate() {
+        assertTrue(dateValidator.isDateBeforeValid(LocalDateTime.now(clock), "23/11/2023 12:00:00"));
+    }
+
+    @Test
+    public void whenValidateAfterDateThenDateMustBeAfterRuleSpecifiedDate() {
+        assertTrue(dateValidator.isDateAfterValid(LocalDateTime.now(clock), "18/11/2023 12:00:00"));
+    }
+
+    @Test
+    public void whenValidateAfterDateThenInvalidIfDateIsBeforeRuleSpecifiedDate() {
+        assertFalse(dateValidator.isDateAfterValid(LocalDateTime.now(clock), "20/11/2023 12:01:00"));
     }
 }
